c6a525ab834560865a9712c942de5f17e8edf57e
mjindra2 <manuelwalterjindra@gmail.com>
2024-11-04 13:31:04 +0100

    Laden fertig

--- src/main/java/Laden.java
+++ src/main/java/Laden.java
@@ -13,7 +13,16 @@ public class Laden implements SuL {
     public Worttrainer strategy(Worttrainer worttrainer2) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("worttrainer.json");
 
+            Worttrainer worttrainer = objectMapper.readValue(inputStream, new TypeReference<Worttrainer>() {});
+            ArrayList<Paar> loadedList = worttrainer.getList();
+            return worttrainer;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
 
     }
 }
