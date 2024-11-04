4ba7447713d1b9b4f15debf9c7278ee8117bfd77
mjindra2 <manuelwalterjindra@gmail.com>
2024-11-04 13:31:29 +0100

    Paar fertig

--- src/main/java/Paar.java
+++ src/main/java/Paar.java
@@ -21,6 +21,42 @@ public class Paar {
             JOptionPane.showMessageDialog(null, "ERROR FALSCHES URL BEI WORT: " + wort);
         }
     }
+    @Override
+    public String toString(){
+        return this.wort;
+    }
+
+    public String getWort(){
+        return wort;
+    }
+
+    public URL getBild(){
+        return bild;
+    }
+
+    public void setWort(String wort) {
+        this.wort = wort;
+    }
+
+    public void setBild(URL bild) {
+        this.bild = bild;
+    }
+
+    public boolean checkWord(String wort){
+        return this.wort.equals(wort);
+    }
+
+    public static boolean isURLValid(String urlString) {
+        try {
+            URL urll = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) urll.openConnection();
+            connection.setRequestMethod("HEAD"); // Use the HEAD method for a lightweight check
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 
 }
